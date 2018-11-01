d8db7837b7c6589f02cb3a2a3d4c28950b5648a2
wsy <35551400@qq.com>
2018-11-01 16:26:46 +0800

    s

---
+++ src/page/home/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home_container">
+
+
+
+
+    <itemcontainer father-component="home"></itemcontainer>
+  </div>
+</template>
+
+<script>
+  import itemcontainer from '../../components/itemcontainer'
+  // import TestSlot from '../../components/header/testSlot'
+
+  export default {
+    name: 'home',
+    components: {
+      itemcontainer
+    },
+    // components: {TestSlot},
+    created() {
+
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  .home_container {
+    color: #FFF;
+    font-size: 12px;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import App from '../App'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'APP',
+      component: App,
+      children:[{
+        path:'',
+        component: r => require.ensure([], () => r(require('../page/home/index')), 'home')
+      }]
     }
   ]
 })
